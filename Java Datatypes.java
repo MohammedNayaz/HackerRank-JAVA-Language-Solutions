21fdffb0159999c6666adf241553aadbbf24dd92
Mohammed Nayaz <64147100+MohammedNayaz@users.noreply.github.com>
2020-10-20 19:54:50 +0530

    Add files via upload

---
+++ Java Datatypes.java
@@ -0,0 +1,40 @@
+//Java Datatypes
+import java.util.*;
+import java.io.*;
+
+
+
+class Solution{
+    public static void main(String []argh)
+    {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                //Complete the code
+    
+                if(x == (short)x)System.out.println("* short");
+                if(x == (int)x)System.out.println("* int");
+                if(x == (long)x)System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
+
+
+
